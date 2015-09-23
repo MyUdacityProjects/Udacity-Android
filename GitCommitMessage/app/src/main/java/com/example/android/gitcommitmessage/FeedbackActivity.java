53f24130ac3a1b159b9c81716e325fda8df150ca
hkasera <hkasera5@gmail.com>
2015-09-23 22:29:24 +0530

    Udacity Project 5 repo set up

---
+++ GitCommitMessage/app/src/main/java/com/example/android/gitcommitmessage/FeedbackActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.gitcommitmessage;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by harshita.k on 23/09/15.
+ */
+public class FeedbackActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.submit_feedback);
+    }
+}
---
+++ GitCommitMessage/app/src/main/java/com/example/android/gitcommitmessage/TipsActivity.java
@@ -0,0 +1,56 @@
+package com.example.android.gitcommitmessage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by harshita.k on 23/09/15.
+ */
+public class TipsActivity extends AppCompatActivity  {
+
+    int tipIndex = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+    }
+
+    public void viewNextTip(View v){
+        if(tipIndex == 5){
+            Button nextBtn = (Button) findViewById(R.id.next_tip);
+            nextBtn.setText(R.string.finish_tip);
+            submitFeedback();
+        }else{
+            tipIndex = tipIndex + 1;
+            displayTipIndex();
+        }
+
+    }
+
+    public void viewPrevTip(View v){
+        if(tipIndex == 1){
+            // Show Toast Message
+        }else{
+            tipIndex = tipIndex - 1;
+            Button nextBtn = (Button) findViewById(R.id.prev_tip);
+            displayTipIndex();
+        }
+    }
+
+    public void displayTipIndex(){
+        TextView tipIndexTextView = (TextView) findViewById(R.id.tip_index);
+        tipIndexTextView.setText("Tip #"+tipIndex);
+    }
+
+    public void submitFeedback(){
+        Intent intent = new Intent(TipsActivity.this, FeedbackActivity.class);
+        startActivity(intent);
+    }
+}
